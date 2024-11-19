462dd1112d6fba2e4c29c85ff319def9b140d2f3
loki20203 <151372360+loki20203@users.noreply.github.com>
2024-11-19 11:37:34 +0200

    Add files via upload

---
+++ Комп'ютер.cs
@@ -0,0 +1,21 @@
+﻿public class Компютер
+{
+    // Основні властивості
+    public string IpАдреса { get; private set; }
+    public int Потужність { get; private set; }
+    public string ТипОс { get; private set; }
+
+    // Конструктор
+    public Компютер(string ipАдреса, int потужність, string типОс)
+    {
+        IpАдреса = ipАдреса;
+        Потужність = потужність;
+        ТипОс = типОс;
+    }
+
+    // Метод для виведення інформації
+    public virtual void ВивестиІнформацію()
+    {
+        Console.WriteLine($"IP: {IpАдреса}, Потужність: {Потужність}, ОС: {ТипОс}");
+    }
+}
